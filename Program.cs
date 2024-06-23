38ad2f264d18f923ac2cf4cc434a46c92cbfd2fc
Adamelbouchiri <matrakmtk@gmail.com>
2024-06-23 22:00:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using System.Data;
+using ContactBusinessLayer;
+
+namespace ContactsConsoleApp_PresentationLayer
+{
+    internal class Program
+    {
+
+        static void testFindContact(int ID)
+        {
+            clsContact contact = clsContact.Find(ID);
+
+            if(contact != null )
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                Console.WriteLine(contact.Email);
+                Console.WriteLine(contact.Phone);
+                Console.WriteLine(contact.Address);
+                Console.WriteLine(contact.DateOfBirth);
+                Console.WriteLine(contact.ImagePath);
+                Console.WriteLine(contact.ContryID);
+            }
+            else
+            {
+                Console.WriteLine("Contact With ID: " + ID + " Is Not Found");
+            }
+        }
+        static void Main(string[] args)
+        {
+            testFindContact(1);
+            Console.ReadKey();
+        }
+    }
+}
